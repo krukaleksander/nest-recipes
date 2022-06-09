1993edfbac0b2e3ffa4a76692323565ffeef0485
aleksander <kruk.aleksander@hotmail.com>
2022-06-09 13:26:17 +0200

    feat: implement pagination

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -3,6 +3,18 @@ import { MockDB } from '../helpers/MockDB';
 import { getIngredients } from './helpers/getIngredients';
 import { IPagination, IRecipe } from '../interfaces';
 
+function chunkArray(arr: IRecipe[], chunkSize: number) {
+  return arr.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / chunkSize);
+
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [];
+    }
+    resultArray[chunkIndex].push(item);
+    return resultArray;
+  }, []);
+}
+
 function getRecipes(
   limit: string,
   page: string,
@@ -10,13 +22,16 @@ function getRecipes(
 ): IRecipe[] | HttpException {
   const limitNumber = +limit;
   const pageNumber = +page;
-  const isPageInRange = limitNumber / database.length > pageNumber - 1;
+  const isPageInRange = database.length / limitNumber > pageNumber - 1;
   if (!limit && !page) return database;
   if (limitNumber > database.length)
     throw new HttpException('limit out of range', HttpStatus.BAD_REQUEST);
   if (limit && !page) return database.slice(0, limitNumber);
   if (limit && page && !isPageInRange)
     throw new HttpException('page out of range', HttpStatus.BAD_REQUEST);
+  if (limit && page && isPageInRange) {
+    return chunkArray(database, +limit)[+page - 1];
+  }
 }
 
 @Injectable()
