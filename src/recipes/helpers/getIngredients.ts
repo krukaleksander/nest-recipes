97e06e81cffe1989b5218c102a609b5450bed2a9
aleksander <kruk.aleksander@hotmail.com>
2022-06-09 08:16:24 +0200

    ref: add extra parameter to getIngredients function and remove redundant getIngredientsTypes function

--- src/recipes/helpers/getIngredients.ts
+++ src/recipes/helpers/getIngredients.ts
@@ -1,12 +1,12 @@
 import { Ingredient, Recipe } from '../../interfaces';
 
-export function getIngredients(MockDB: Recipe[]) {
+export function getIngredients(MockDB: Recipe[], key: string) {
   const db = MockDB;
   const arrOfAllIngredients: string[] = [];
   db.forEach((recipe: Recipe) => {
     const { ingredients } = recipe;
     ingredients.forEach((ingredient: Ingredient) =>
-      arrOfAllIngredients.push(ingredient.name),
+      arrOfAllIngredients.push(ingredient[key]),
     );
   });
   return [...new Set(arrOfAllIngredients.flat(1))];
--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,27 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { MockDB } from '../helpers/MockDB';
 import { getIngredients } from './helpers/getIngredients';
-import { Ingredient, Recipe } from '../interfaces';
-
-function getIngredientsTypes(MockDB: Recipe[]) {
-  const db = MockDB;
-  const arrOfAllIngredientsTypes: string[] = [];
-  db.forEach((recipe: Recipe) => {
-    const { ingredients } = recipe;
-    ingredients.forEach((ingredient: Ingredient) =>
-      arrOfAllIngredientsTypes.push(ingredient.type),
-    );
-  });
-  return [...new Set(arrOfAllIngredientsTypes.flat(1))];
-}
 
 @Injectable()
 export class RecipesService {
   getListOfUniqueIngredients(): string[] {
-    return getIngredients(MockDB);
+    return getIngredients(MockDB, 'name');
   }
 
   getListOfIngredientsTypes(): string[] {
-    return getIngredientsTypes(MockDB);
+    return getIngredients(MockDB, 'type');
   }
 }
