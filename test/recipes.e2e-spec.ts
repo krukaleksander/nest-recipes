1798ecd69532262632a6a8bd8cd6d28d62b8e8f4
aleksander <kruk.aleksander@hotmail.com>
2022-06-09 15:57:26 +0200

    test: /recipes/single (GET), should return 204 no content if id is not in db

--- test/recipes.e2e-spec.ts
+++ test/recipes.e2e-spec.ts
@@ -119,12 +119,16 @@ describe('Recipes (e2e)', () => {
   });
   describe('/recipes/single (GET)', () => {
     const endpoint = '/recipes/single';
-    it('should return status 200', () => {
-      return request(app.getHttpServer()).get(endpoint).expect(200);
+    it('should return status 204, without any id', () => {
+      return request(app.getHttpServer()).get(endpoint).expect(204);
     });
     it('should return one element if valid id is passed', async () => {
       const { body: response } = await getFromServer(endpoint, { id: 0 });
       expect(response).toHaveLength(1);
+    });
+    it('should return 204 no content if id is not in db', async () => {
+      const response = await getFromServer(endpoint, { id: 999 });
+      expect(response.statusCode).toEqual(204);
     });
   });
 });
