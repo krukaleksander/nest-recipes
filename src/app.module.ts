8daf279733c0be8cc1e874e55f16356d40a76f44
aleksander <kruk.aleksander@hotmail.com>
2022-06-11 09:45:48 +0200

    feat: use DataInitService in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
+import { DataInitService } from './helpers/DataInit';
 
 @Module({
   imports: [RecipesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DataInitService],
 })
 export class AppModule {}
