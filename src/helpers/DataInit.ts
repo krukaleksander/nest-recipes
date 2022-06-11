f8e5ddceb20db83c752b7bcc89bc56b7d35cc30e
aleksander <kruk.aleksander@hotmail.com>
2022-06-11 10:36:21 +0200

    feat: initial migration recipes to db without ingredients

--- src/helpers/DataInit.ts
+++ src/helpers/DataInit.ts
@@ -1,8 +1,25 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-
+import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Recipe } from '../database/entities/Recipe.entity';
+import { MockDB } from './MockDB';
+import { IRecipe } from '../interfaces';
 @Injectable()
 export class DataInitService implements OnApplicationBootstrap {
-  onApplicationBootstrap() {
+  constructor(
+    @Inject('RECIPE_REPOSITORY')
+    private recipeRepository: Repository<Recipe>,
+  ) {}
+  async onApplicationBootstrap() {
     console.log('app is starting');
+    try {
+      await Promise.all(
+        MockDB.map(
+          async (recipe: IRecipe) => await this.recipeRepository.save(recipe),
+        ),
+      );
+      await this.recipeRepository.save(MockDB[0]);
+    } catch (error) {
+      throw error;
+    }
   }
 }
