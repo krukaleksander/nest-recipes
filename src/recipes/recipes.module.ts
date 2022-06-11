0a0f3a401fda641e16b6eac03878d6e24c340159
aleksander <kruk.aleksander@hotmail.com>
2022-06-11 13:02:39 +0200

    feat: remove mock from get all recipes

--- src/recipes/recipes.module.ts
+++ src/recipes/recipes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
+import { recipeProviders } from './repos/recipe.providers';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [RecipesController],
-  providers: [RecipesService],
+  providers: [RecipesService, ...recipeProviders],
 })
 export class RecipesModule {}
